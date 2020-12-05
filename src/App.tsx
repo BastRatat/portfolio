7ff82c3b2cdb03d0e156f2801a47c0266c2b5090
Bastien <bastien.ratat@gmail.com>
2020-12-05 23:34:04 +0100

    create a type animation on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@
  // declare a card object for each tab with titles and content
 
 import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 import './App.css';
 import  { tabsContentEnglish } from './utils/tabsContent'
 import NavigationBar from './components/header/NavigationBar.tsx'
 import HomeCard from './components/header/HomeCard.tsx'
-import ProfilePicture from './components/header/ProfilePicture.tsx'
+import bastienPicture from './images/myself.jpg'
 
 function App() {
   
   console.log(tabsContentEnglish)
 
+  const [homeMessage, setHomeMessage] = useState<boolean>(true)
   const [homeHovered, setHomeHovered] = useState<boolean>(false)
   const [projectHovered, setProjectHovered] = useState<boolean>(false)
   const [contactHovered, setContactHovered] = useState<boolean>(false)
@@ -20,16 +22,19 @@ function App() {
   const handleHover = (tabName) => {
     switch(tabName) {
       case "Home":
+        setHomeMessage(false)
         setProjectHovered(false)
         setContactHovered(false)
         setHomeHovered(true)
         break
       case "Projects":
+        setHomeMessage(false)
         setContactHovered(false)
         setHomeHovered(false)
         setProjectHovered(true)
         break
       case "Contact":
+        setHomeMessage(false)
         setProjectHovered(false)
         setHomeHovered(false)
         setContactHovered(true)
@@ -42,6 +47,16 @@ function App() {
       <header>
         <NavigationBar handleHover={handleHover} />
       </header>
+      {homeMessage && (
+        <Container fluid className="home-message">
+              <h2 className="text-center p-3">Bastien Ratat, <span className="span-style">26</span></h2>
+              <div className="d-flex align-items-center justify-content-center align-items-center p-3">
+                <h5 className="typewriter">
+                  <span style={{color: '#fc5185'}}>{"<h1>"}</span>Full stack developer<span style={{color: '#fc5185'}}>{"<h1>"}</span>
+                </h5>
+              </div>
+        </Container>
+      )}
       {homeHovered && (
         <HomeCard picture={1} title={tabsContentEnglish.home.title} text={tabsContentEnglish.home.text} />
       )}
