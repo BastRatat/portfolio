ca6025e7dc301acd933a12c61c8f859fbaf6a861
Bastien <bastien.ratat@gmail.com>
2020-12-21 18:26:18 +0100

    implemented emailjs within my contact form

--- src/components/body/Contact/ContactForm.tsx
+++ src/components/body/Contact/ContactForm.tsx
@@ -4,122 +4,49 @@
 import React, { useState } from 'react';
 import { Container, Alert, Button } from 'react-bootstrap';
 import { Formik, Field, Form, useField } from 'formik';
-
-interface formDataProps {
-  firstName: string,
-  lastName: string,
-  email: string,
-  body: string
-}
-
-const MyTextArea = ({...props}) => {
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <textarea className="text-area" {...field} {...props} />
-      {meta.touched && meta.error ? (
-          <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
-const formInitialValues = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  body: ''
-}
+import emailjs from 'emailjs-com'
 
 const ContactForm:React.FunctionComponent = () => {
   
   const [hasError, setHasError] = useState<boolean>(false)
   const [messageSent, setMessageSent] = useState<boolean>(false)
 
-  const handleSubmit = async (formData:formDataProps) => {
-    try {
-      console.log(formData)
-    } catch(error) {
-      console.error(error)
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_ky0zz0b', 'template_ebua76n', e.target, 'user_rmnvJll19tNuCfacJBA9A')
+      .then((result) => {
+        setMessageSent(true)
+      }, (error) => {
+        setHasError(true)
+      });
+    e.target.reset()
   }
+
   
   return (
-    <Container className="d-flex justify-content-center m-2">
-      {hasError && (
-        <Alert variant='danger' className="alert text-center">Error</Alert>
-      )}
-      {messageSent && (
-        <Alert variant='success' className="alert text-center">Message envoyé</Alert>
-      )}
-      <Formik
-        initialValues={formInitialValues}
-        onSubmit={
-          (values, actions) => {
-            const dataToAPI = {
-              firstName: `${values.firstName}`,
-              lastName: `${values.lastName}`,
-              email: `${values.email}`,
-              body: `${values.body}`,
-            }
-            handleSubmit(dataToAPI)
-            actions.setSubmitting(false)
-            actions.resetForm()
-          }
-        }
-      >
-        {({errors, touched, handleSubmit, handleChange, handleBlur, values }) => (
-          <Form>
-            <div className="fields m-2">
-              <Field
-                type="text"
-                className="input-fields"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.firstName}
-                name="firstName"
-                placeholder="Prénom"
-              />
-            </div>
-            <div className="fields m-2">
-              <Field
-                type="text"
-                className="input-fields"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.lastName}
-                name="lastName"
-                placeholder="Nom de famille"
-              />              
-            </div>
-            <div className="fields m-2">
-              <Field
-                type="email"
-                className="input-fields"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                name="email"
-                placeholder="Email"
-              />              
-            </div>
-            <div className="fields m-2">
-              <MyTextArea
-                onChange={handleChange}
-                className="text-area"
-                onBlur={handleBlur}
-                value={values.body}
-                name="body"
-                rows="3"
-                placeholder="Votre message"
-              />              
-            </div>
-            <div className="m-3 d-flex justify-content-center">
-              <Button type="submit" className="tech-btn">Envoyer</Button>
-            </div>
-          </Form>
+    <Container className="d-flex justify-content-center my-3">
+      <form onSubmit={sendEmail}>
+        {hasError && (
+          <div>
+            <Alert variant='danger' className="alert text-center">Service indisponible</Alert>
+          </div>
         )}
-      </Formik>
+        {messageSent && (
+          <div>
+            <Alert variant='success' className="alert text-center">Message envoyé</Alert>
+          </div>
+        )}
+        <div className="fields m-2">
+          <input type="text" name="name" className="input-fields" />
+        </div>
+        <div className="fields m-2">
+          <input type="email" name="email" className="input-fields" />
+        </div>
+        <div className="fields m-2">
+          <textarea name="body" className="text-area"/>
+        </div>
+        <Button type="submit" className="tech-btn ml-3 mb-3">Envoyer</Button>
+      </form>
     </Container>
   )
 }
