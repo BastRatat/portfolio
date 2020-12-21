3cb965ff00105046461ba6d4a46bebd326ee61d9
Bastien <bastien.ratat@gmail.com>
2020-12-21 18:27:20 +0100

    typo

--- src/components/body/Contact/ContactDescription.tsx
+++ src/components/body/Contact/ContactDescription.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactDescription:React.FunctionComponent = () => {
   return (
     <article className="mt-2"> 
-      <h4 className="p-2 contact-title">Développement d'application web de qualité</h4>
+      <h4 className="p-2 contact-title">Développement d'applications web de qualité</h4>
       <p className="p-3 contact-description">Muni d'une grande adaptabilité à de nombreux languages et technologies informatiques, je suis en mesure de développer des applications web performantes. Que vous soyez un particulier ou une entreprise, n'hésitez pas à me contacter pour échanger sur les projets de vos rêves.</p>
     </article>
   )
